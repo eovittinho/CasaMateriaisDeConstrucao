9f8b138ea9399518fa8a48dbe3f99141ad4a8dcd
eovittinho <victorthulio001@gmail.com>
2025-04-15 16:48:20 -0300

    Construcao

---
+++ Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+
+app.UseDefaultFiles(); 
+app.UseStaticFiles();
+
+app.MapGet("/sobre", async context =>
+{
+await context.Response.SendFileAsync("wwwroot/sobre.html");
+});
